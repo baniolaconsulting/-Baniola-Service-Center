d8827ad320dab22d9a54235ebcc52fb5db5a9e7e
Haythem Saad <h.saad1905@myu.universitecentrale.tn>
2022-04-21 14:59:22 +0100

    Composants + Bootstrap

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/baniola-service-center'),
+      dir: require('path').join(__dirname, './coverage/baniola-services-center'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Composants/home/home.component';
+import { LoginComponent } from './Composants/login/login.component';
+import { ListingComponent } from './Composants/listing/listing.component';
+import { AjoutComponent } from './Composants/ajout/ajout.component';
+import { InterventionsComponent } from './Composants/interventions/interventions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    ListingComponent,
+    AjoutComponent,
+    InterventionsComponent
   ],
   imports: [
     BrowserModule,
